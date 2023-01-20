9b74e21572da28316e62e9cf9c55629d5dba424a
anwarul <anwarulhoque032@gmail.com>
2023-01-21 00:08:54 +0600

    added mongodb and create API

--- index.js
+++ index.js
@@ -15,12 +15,21 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
+    try {
+        const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
 
+        app.get('/appointmentOptions', async(req, res) =>{ 
+            const query = {};
+            const options = await appointmentOptionCollection.find(query).toArray();
+            res.send(options);
+        })
+    }
+    finally { }
 }
 run().catch(console.log)
 
 
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
     res.send('doctors portal server is running...')
 })
 
